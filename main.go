4a851f2f43572477efafbca20528f5ec1bfced0e
Liam Hernandez <liamhernandez>
2024-04-14 17:06:36 -0700

    First HTML form and quick Go to serve the file.

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	log.Println("Server listening on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
